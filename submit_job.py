4654c64c80481843350cf8f6f098dcac1c68dd7a
torresal <alexander.p.torres@jol.nasa.gov>
2021-02-25 22:44:26 -0800

    added default=job-ingest_lpdaac_prods:dev

--- submit_job.py
+++ submit_job.py
@@ -56,9 +56,9 @@ def parse_job_tags(tag_string):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-j', '--job_name', help='Job name',
-                        dest='job_name', required=True, default="job-ingest_lpdaac_prod:dev")
+                        dest='job_name', required=True, default="job-ingest_lpdaac_prods:dev")
     parser.add_argument(
-        '-p', '--params', help='Input params dict', dest='params', required=True)
+        '-p', '--params', help='Input params dict', dest='params', required=True, default={"s3_lpdaac_email_bucket": "ava-ses-emails"})
     parser.add_argument('-v', '--version', help='release version, eg "master" or "release-20180615"',
                         dest='version', required=False, default='dev')
     parser.add_argument('-q', '--queue', help='Job queue', dest='queue',
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--priority', help='Job priority',
                         dest='priority', required=False, default='5')
     parser.add_argument('-g', '--tags', help='Job tags. Use a comma separated list for more than one',
-                        dest='tags', required=False, default='neic_event_aoi')
+                        dest='tags', required=False, default='automated-granule-ingest')
     args = parser.parse_args()
     main(args.job_name, args.params, args.version,
          args.queue, args.priority, args.tags)
